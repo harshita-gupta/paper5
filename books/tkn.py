06fd39d126c94b5bdd1a21a265e7538e790d9c87
harshita-gupta <gupta.harshita.001@gmail.com>
2017-03-16 22:24:19 -0700

    token file for emma created

--- books/tkn.py
+++ books/tkn.py
@@ -7,35 +7,14 @@ marquez = 'marquez/marquez'
 ulysses = 'ulysses/ulysses'
 emma = 'emma/emma'
 
-for i in range(1, 19):
-    filename = emma + "%03d" % i
-    with open(filename, 'r') as myfile:
-        chapter = myfile.read()
+with open(emma + "token", 'w') as writeFile:
+    for i in range(1, 56):
+        filename = emma + "%03d" % i
+        with open(filename, 'r') as myfile:
+            chapter = myfile.read()
 
-    # book = "hello it's me. um hi."
-    annotated = client.annotate(chapter)
-    for sentence in annotated:
-        for token in sentence:
-            print token
-# print annotated
-# for sentence in annotated.sentences:
-#     print('sentence', sentence)
-#     for token in sentence:
-#         print(token.word, token.lemma, token.pos, token.ner)
-
-# from nltk.tokenize.stanford import StanfordTokenizer
-# # from nltk import word_tokenize
-# # from nltk.tag import StanfordPOSTagger
-# import sys
-# reload(sys)
-# sys.setdefaultencoding("utf-8")
-
-# mypath = '/Users/Harshita/Documents/Harvard/17 Freshman Spring/HUM10/paper5/'
-# # mypath = mypath + 'stanford-dirs/stanford-postagger-2016-10-31/'
-
-# # jar = mypath + 'stanford-postagger.jar'
-# # model = mypath + 'models/english-left3words-distsim.tagger'
-
-# # pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8')
-
-# print StanfordTokenizer().tokenize(unicode(book))
+        annotated = client.annotate(chapter)
+        for sentence in annotated:
+            for token in sentence:
+                writeFile.write(token.word)
+                writeFile.write("\n")
