809b676d897aae6c01b2de79d5124e832d48d7e2
harshita-gupta <gupta.harshita.001@gmail.com>
2017-04-25 04:51:58 -0400

    code for women inclusion and exclusion network graphs

--- lda_network_m.py
+++ lda_network_m.py
@@ -13,24 +13,21 @@ import sys
 import os
 
 NUMTERMS = 50
-valthresh = 0.0035
+valthresh = 0.0040
 
 womenwords = set(['daughter', 'ladi', 'princess', 'wife', 'women', 'woman',
                   'mother', 'girl', 'sister'])
 
-#############
-# cf. http://nbviewer.ipython.org/github/sgsinclair/alta/blob/master/ipynb/TopicModelling.ipynb#Graphing-Topic-Terms
 
+#############
 def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
 
-    # create a new graph and size it
     G = nx.Graph()
     plt.figure(figsize=(50, 50))
 
-    # generate the edges
     for i in range(0, lda.num_topics):
-        # topicLabel = "topic " + str(i)
-        terms = [term for term, val in lda.show_topic(i, num_terms) if val > valthresh]
+        terms = [term for term, val in lda.show_topic(i, num_terms)
+                 if val > valthresh]
         if "competitor" in terms:
             continue
         if "rebukescoupl" in terms:
@@ -45,14 +42,11 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
                     else:
                         G.add_edge(term, term2)
 
-    # cf.
-    # http://networkx.lanl.gov/reference/drawing.html#module-networkx.drawing.layout
     pos = nx.spring_layout(G, k=0.30, iterations=80000)
 
     g = G.subgraph([term for term, _ in pos.items()])
     nx.draw_networkx_labels(g, pos, font_size=25, alpha=0.9)
 
-    # plot edges
     nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='g',
                            alpha=0.3)
 
@@ -61,15 +55,11 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
             nx.draw_networkx_edges(G, pos, edgelist=nx.edges(G, nbunch=n),
                                    edge_color='r', weight=1.2)
 
-    print len(G)
-
     plt.axis('off')
     plt.savefig(outfile % "-with-women")
 
     plt.clf()
 
-
-    ##########
     # GRAPH 2
 
     nodesToDel = []
@@ -81,23 +71,41 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
 
     nodesToDel = set(nodesToDel)
 
-    for node in nodesToDel:
-        G.remove_node(node)
-
     g = G.subgraph([term for term, _ in pos.items() if term not in nodesToDel])
-    nx.draw_networkx_labels(g, pos, font_size=25, alpha=0.9)
+    nx.draw_networkx_labels(g, pos, font_size=40, alpha=0.9)
 
-    # plot edges
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='g',
+    nx.draw_networkx_edges(g, pos, edgelist=g.edges(), edge_color='g',
                            alpha=0.3)
 
-
-    print len(G)
     plt.axis('off')
     plt.savefig(outfile % "-no-women")
 
-    # plt.show()
+    plt.clf()
 
+    # GRAPH 3
+
+    for womanword in womenwords:
+        if womanword in G:
+            nodesToKeep = set(list(sum(G.edges(womanword), ())))
+            g = G.subgraph([term for term, _ in pos.items()
+                            if term in nodesToKeep])
+            nx.draw_networkx_labels(g, pos, font_size=40, alpha=0.9)
+            nx.draw_networkx_edges(g, pos, edgelist=g.edges(), edge_color='r',
+                                   alpha=0.3)
+            plt.axis('off')
+            plt.savefig(outfile % ("-" + womanword))
+
+            plt.clf()
+
+            g = G.subgraph([term for term, _ in pos.items()
+                            if term not in nodesToKeep])
+            nx.draw_networkx_labels(g, pos, font_size=40, alpha=0.9)
+            nx.draw_networkx_edges(g, pos, edgelist=g.edges(), edge_color='g',
+                                   alpha=0.3)
+            plt.axis('off')
+            plt.savefig(outfile % ("-no-" + womanword))
+
+            plt.clf()
 
 
 if len(sys.argv) < 2:
@@ -108,7 +116,7 @@ if len(sys.argv) < 2:
 path, file = os.path.split(sys.argv[1])
 corpusname = file.split(".")[0]
 outdir = "networks/dense/" + path.replace("/", "-").replace("-out", "").replace("books-genji-", "")
-outfile = outdir + "-network_m-w%s-%s-w%s.png" % (NUMTERMS, "%s", sys.argv[2])
+outfile = outdir + "-network_m-w%s-%s-%s-%sthresh.png" % (NUMTERMS, "%s", sys.argv[2], valthresh)
 
 model = LdaModel.load(sys.argv[1])
 
