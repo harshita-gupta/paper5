d62cb9e9cd14ac9030e0768b8a12cf60f4af5e7c
harshita-gupta <gupta.harshita.001@gmail.com>
2017-04-26 18:45:08 -0400

    added updated topics

--- lda_print_topics.py
+++ lda_print_topics.py
@@ -12,10 +12,14 @@ path, file = os.path.split(sys.argv[1])
 corpusname = file.split(".")[0]
 lda = LdaModel.load(sys.argv[1])
 
+topics = 1
 for i in range(0, lda.num_topics):
         terms = [term for term, val in lda.show_topic(i, NUMTERMS)
                  if val > valthresh]
-        termstring = str(i) + " "
-        for term in terms:
-            termstring += term + " "
-        print termstring
+        if terms:
+            termstring = "Topic " + str(topics) + ": "
+            topics += 1
+            for term in terms:
+                termstring += term + " "
+            print termstring
+            print ""
