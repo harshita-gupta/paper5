270138fba53bbf2246380075d720424239f5a258
harshita-gupta <gupta.harshita.001@gmail.com>
2017-04-26 13:38:45 -0400

    added pickling for pos so that models can be intervened in with more ease

--- lda_network_m.py
+++ lda_network_m.py
@@ -8,10 +8,12 @@ from gensim.models import LdaModel
 import networkx as nx
 import matplotlib
 import pickle
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 import sys
 import os
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+fromPickle = True
 
 NUMTERMS = 50
 valthresh = 0.0040
@@ -19,7 +21,9 @@ valthresh = 0.0040
 womenwords = set(['daughter', 'ladi', 'princess', 'wife', 'women', 'woman',
                   'mother', 'girl', 'sister'])
 
-menwords = set(['son'])
+mendependentwomen = set(['daughter', 'wife', 'mother', 'sister'])
+
+menwords = set(['son', 'prince'])
 
 testwords = womenwords
 
@@ -47,8 +51,13 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
                     else:
                         G.add_edge(term, term2)
 
-    pos = nx.spring_layout(G, k=0.30, iterations=80000)
-    pickle.dump(pos, outfile % "positions-" + ".dict")
+    if fromPickle:
+        with open((outfile % "positions-") + ".d", 'wb') as f:
+            pos = pickle.load(f)
+    else:
+        pos = nx.spring_layout(G, k=0.30, iterations=80000)
+        with open((outfile % "positions-") + ".d", 'wb') as f:
+            pickle.dump(pos, f, pickle.HIGHEST_PROTOCOL)
 
     g = G.subgraph([term for term, _ in pos.items()])
     nx.draw_networkx_labels(g, pos, font_size=25, alpha=0.9)
