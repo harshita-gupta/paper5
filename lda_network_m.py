8d5651400c13dca34b78a64d74e3e71ee6b40773
harshita-gupta <gupta.harshita.001@gmail.com>
2017-04-26 23:05:21 -0400

    blue male edges

--- lda_network_m.py
+++ lda_network_m.py
@@ -29,7 +29,7 @@ menwords = set(['son', 'prince', 'father', 'emperor', 'minister',
                 'lord', 'man', 'husband'])
 
 feelingthinking = set(['feel', 'think', 'felt', 'thought', 'tear',
-                       'emot'])
+                       'emot', 'die'])
 
 performanceappearance = set(['perform', 'play', 'dress', 'robe', 'music'])
 
@@ -69,22 +69,28 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
         with open((outfile % "positions-") + ".d", 'wb') as f:
             pickle.dump(pos, f)
 
-    def drawGraph(g, font, color1='m', color2='r'):
+    def drawGraph(g, font, color1='m', color2='r', c3='b'):
         nx.draw_networkx_labels(g, pos, font_size=font, alpha=1.0)
         nx.draw_networkx_edges(g, pos, edgelist=g.edges(), edge_color='g',
-                               alpha=0.3)
-
+                               alpha=0.4)
         for n in g:
             if n in singularwomen:
                 nx.draw_networkx_edges(g, pos, edgelist=nx.edges(g, nbunch=n),
-                                       edge_color=color1, weight=1.2)
+                                       edge_color=color1, weight=1.0)
             if n in mendependentwomen:
                 nx.draw_networkx_edges(g, pos, edgelist=nx.edges(g, nbunch=n),
-                                       edge_color=color2, weight=1.2)
+                                       edge_color=color2, weight=1.0)
+            if n in menwords:
+                nx.draw_networkx_edges(g, pos, edgelist=nx.edges(g, nbunch=n),
+                                       edge_color='b', weight=1.0)
+                medges = nx.edges(g, nbunch=n)
+                medges = [(n1, n2) for (n1, n2) in medges if n2 in womenwords]
+                nx.draw_networkx_edges(g, pos, edgelist=medges,
+                                       edge_color='y', weight=1.0)
 
     # GRAPH WITH EVERYTHING
     g = G.subgraph([term for term, _ in pos.items()])
-    drawGraph(g, 25, 'r', 'r')
+    drawGraph(g, 25, 'r', 'r', 'r')
     save_clear("full")
     drawGraph(g, 25)
     save_clear("full-split")
@@ -121,17 +127,17 @@ def graph_terms_to_topics(lda, outfile, num_terms=NUMTERMS):
 
                 save_clear(("no-" + testword))
 
-    removespecificcategory("womenwords", womenwords)
-    removeindivwordscat(womenwords)
-    removespecificcategory("feelingthinking", feelingthinking)
-    removeindivwordscat(feelingthinking)
-    removespecificcategory("mendependentwomen", mendependentwomen)
-    removespecificcategory("menwords", menwords)
-    removeindivwordscat(menwords)
-    removespecificcategory("performanceappearance", performanceappearance)
-    removeindivwordscat(performanceappearance)
-    removespecificcategory("dutypalace", dutypalace)
-    removeindivwordscat(dutypalace)
+    # removespecificcategory("womenwords", womenwords)
+    # removeindivwordscat(womenwords)
+    # removespecificcategory("feelingthinking", feelingthinking)
+    # removeindivwordscat(feelingthinking)
+    # removespecificcategory("mendependentwomen", mendependentwomen)
+    # removespecificcategory("menwords", menwords)
+    # removeindivwordscat(menwords)
+    # removespecificcategory("performanceappearance", performanceappearance)
+    # removeindivwordscat(performanceappearance)
+    # removespecificcategory("dutypalace", dutypalace)
+    # removeindivwordscat(dutypalace)
 
 
 if len(sys.argv) < 2:
